05b1da4e89cbc3c14647d176a0edf1bc6cf3611b
ilyasid1990 <79026851552@yandex.ru>
2026-07-16 22:30:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import express, {type Express} from "express";
 import {setupApp} from "./setup-app.js";
 import {SETTINGS} from './core/config/settings.js';
+import { runDB } from './db/mongo.db.js';
 
 
-const app: Express = express();
 
-setupApp(app);
+const bootstrap = async () => {
+    const app: Express = express();
+    setupApp(app);
+    const PORT = SETTINGS.PORT;
 
-const PORT = SETTINGS.PORT;
+    await runDB(SETTINGS.MONGO_URL);
+
+    app.listen(PORT, ():void => {
+        console.log(`Example app listening on port ${PORT}`);
+    });
+    return app;
+};
+
+bootstrap();
 
 
-app.listen(PORT, (): void => {
-    console.log(`Example app listening on port ${PORT}`);
-});
+
